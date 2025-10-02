6f044c1e2e9e95d01e706279249840dfd5c959bb
aio <aiminsupremacy@gmail.com>
2025-10-03 07:02:42 +0800

    added nav semantics to header-bottom and changed sidebar container to aside, also transferred the position absolute to their respective containers for cleaner look. Additionally, finished the bottom header + temporarily made the body grid to get rid of the padding

--- HTML/scripts/data/video-info.js
+++ HTML/scripts/data/video-info.js
@@ -62,5 +62,141 @@ export const videoInfo = [
     videoTitle: "We're Suing Roblox",
     videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
     videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
+  },
+  {
+    id: '',
+    thumbnailImage: '../images/thumbnails/hq720.avif',
+    profilePicture: '../images/profile-pictures/channels4_profile.jpg',
+    videoTitle: "We're Suing Roblox",
+    videoAuthor: 'Law By Mike <span class="checkbox">&#10003;</span>',
+    videoStats: '5.6M views &#8226; 18 hours ago'
   }
 ];
